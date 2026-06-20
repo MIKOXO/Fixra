b36387ef70e7bbe1d020b4f3939bb5a134e7d719
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-06-20 12:09:47 -0300

    Add notification and escalation worker initialization in server

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ import http from 'node:http';
 import app from './app.js';
 import connectDB from './config/db.js';
 import { initSocket } from './sockets/index.js';
+import { notificationWorker } from './queues/processors/notification.processor.js';
+import { escalationWorker } from './queues/processors/escalation.processor.js';
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +13,13 @@ const start = async () => {
 
   const server = http.createServer(app);
   initSocket(server);
+
+  if (notificationWorker) {
+    console.log('Notification worker started');
+  }
+  if (escalationWorker) {
+    console.log('Escalation worker started');
+  }
 
   server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
