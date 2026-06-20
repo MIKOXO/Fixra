52fea354c64f88b8731ddf2e430b64c41de8e294
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-06-20 15:32:44 -0300

    Add Firebase configuration for initializing admin SDK

---
+++ server/src/config/firebase.js
@@ -0,0 +1,15 @@
+import admin from 'firebase-admin';
+
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+
+let firebase = null;
+
+if (projectId && privateKey && clientEmail) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert({ projectId, privateKey, clientEmail }),
+  });
+}
+
+export default firebase;
