6847e1029b70c3396834c7ac98aeee9dbbf775ad
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-06-20 10:59:02 -0300

    Add job routes endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ import passport from 'passport';
 import configurePassport from './config/passport.js';
 import authRoutes from './routes/auth.routes.js';
 import inviteRoutes from './routes/invite.routes.js';
+import jobRoutes from './routes/job.routes.js';
 import propertyRoutes from './routes/property.routes.js';
 import ticketRoutes from './routes/ticket.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -47,6 +48,7 @@ app.get('/health', (_req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/invites', inviteRoutes);
+app.use('/api/jobs', jobRoutes);
 app.use('/api/properties', propertyRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/users', userRoutes);
