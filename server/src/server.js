cec59196e298c8fabebb78c1eb143d96043bbaa3
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-06-20 11:31:34 -0300

    Integrate socket.io support

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 import 'dotenv/config';
+import http from 'node:http';
 import app from './app.js';
 import connectDB from './config/db.js';
+import { initSocket } from './sockets/index.js';
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
   await connectDB();
-  app.listen(port, () => {
+
+  const server = http.createServer(app);
+  initSocket(server);
+
+  server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
 };
