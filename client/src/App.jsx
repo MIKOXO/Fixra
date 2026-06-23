0b23ead48137b68966a8b2ad4c9cf7a73a9d7dee
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-06-23 04:58:29 -0300

    Integrate RouterProvider into App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,8 @@
-const App = () => null;
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
