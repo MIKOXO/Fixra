286cdd85583b84dac1e1f2e503dc2739ccab1ac5
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-07-09 17:25:24 -0300

    Add reducers for analytics, tickets, and properties to the store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import analyticsReducer from './slices/analyticsSlice';
+import ticketReducer from './slices/ticketSlice';
+import propertyReducer from './slices/propertySlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    analytics: analyticsReducer,
+    tickets: ticketReducer,
+    properties: propertyReducer,
   },
 });
 
