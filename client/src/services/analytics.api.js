5afc110f846df2bcca53fd4666ae09f645f2b26c
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-07-11 10:05:52 -0300

    Generalize analytics service parameters

--- client/src/services/analytics.api.js
+++ client/src/services/analytics.api.js
@@ -1,12 +1,13 @@
 import api from './api';
 
-export const getResolutionTime = (propertyId) =>
-  api
-    .get('/analytics/resolution-time', { params: propertyId ? { propertyId } : undefined })
-    .then((res) => res.data);
+export const getResolutionTime = (params) =>
+  api.get('/analytics/resolution-time', { params }).then((res) => res.data);
 
-export const getCostPerProperty = () =>
-  api.get('/analytics/cost-per-property').then((res) => res.data);
+export const getTechnicianPerformance = (params) =>
+  api.get('/analytics/technician-performance', { params }).then((res) => res.data);
 
-export const getMaintenanceFrequency = () =>
-  api.get('/analytics/maintenance-frequency').then((res) => res.data);
+export const getCostPerProperty = (params) =>
+  api.get('/analytics/cost-per-property', { params }).then((res) => res.data);
+
+export const getMaintenanceFrequency = (params) =>
+  api.get('/analytics/maintenance-frequency', { params }).then((res) => res.data);
