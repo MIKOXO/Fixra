74fe9b05b933468e9b3d24d3571aafee650438d6
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-07-21 15:48:19 -0300

    Add contractor performance api service

--- client/src/services/analytics.api.js
+++ client/src/services/analytics.api.js
@@ -4,10 +4,17 @@ export const getResolutionTime = (params) =>
   api.get('/analytics/resolution-time', { params }).then((res) => res.data);
 
 export const getTechnicianPerformance = (params) =>
-  api.get('/analytics/technician-performance', { params }).then((res) => res.data);
+  api
+    .get('/analytics/technician-performance', { params })
+    .then((res) => res.data);
 
 export const getCostPerProperty = (params) =>
   api.get('/analytics/cost-per-property', { params }).then((res) => res.data);
 
 export const getMaintenanceFrequency = (params) =>
-  api.get('/analytics/maintenance-frequency', { params }).then((res) => res.data);
+  api
+    .get('/analytics/maintenance-frequency', { params })
+    .then((res) => res.data);
+
+export const getContractorPerformance = () =>
+  api.get('/analytics/contractor-performance').then((res) => res.data);
